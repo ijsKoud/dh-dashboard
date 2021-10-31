e0ab1e4fc70a42568a3f4e2b28dbdcb44ef0f9d8
DaanGamesDG <daan@daangamesdg.wtf>
2021-10-31 11:54:50 +0100

    chore: adding boilerplate code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/index.scss";
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+	return (
+		<>
+			<Component {...pageProps} />
+		</>
+	);
+};
 
-export default MyApp
+export default App;
