fec94ef5e0fb02c6166aebbb7197519190ce5878
DaanGamesDG <daan@daangamesdg.wtf>
2021-11-03 20:45:25 +0100

    feat: moving level component to own file

---
+++ components/Leveling/Level.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
+import type { LeaderboardStat } from "../../lib";
+
+const Level: React.FC<LeaderboardStat & { breakline: boolean }> = ({ i, level, breakline }) => {
+	const getColour = (index: number): string =>
+		index === 0 ? "#D99D46" : index === 1 ? "#989898" : index === 2 ? "#AD7346" : "#50555A";
+
+	const getProgress = () => {
+		return (level.xp / (level.level * 75)) * 100;
+	};
+
+	return (
+		<div>
+			<div className="leveling-item">
+				<h1
+					className="leveling__item-title"
+					style={{
+						background: getColour(i),
+					}}>
+					{i + 1}
+				</h1>
+				<p className="leveling__item-username">{level.tag}</p>
+				<CircularProgressbar
+					className="progress-ring"
+					value={getProgress()}
+					text={`Level ${level.level}`}
+					styles={buildStyles({
+						trailColor: "#50555A",
+						pathColor: "#46CBF1",
+						textColor: "#46CBF1",
+					})}
+				/>
+			</div>
+			{breakline && <div className="break-line" />}
+		</div>
+	);
+};
+
+export default Level;
--- components/Leveling/index.tsx
+++ components/Leveling/index.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 import { fetch, LeaderboardStat } from "../../lib";
+import Level from "./Level";
 
 const Leveling: React.FC = () => {
 	const [stats, setStats] = useState<LeaderboardStat[]>([]);
@@ -15,41 +15,12 @@ const Leveling: React.FC = () => {
 		return () => cancel();
 	}, []);
 
-	const getColour = (index: number): string =>
-		index === 0 ? "#D99D46" : index === 1 ? "#989898" : index === 2 ? "#AD7346" : "#50555A";
-
-	const getProgress = (level: LeaderboardStat) => {
-		return (level.level.xp / (level.level.level * 75)) * 100;
-	};
-
 	return (
 		<div className="leveling">
 			<h1 className="leveling-title">Leveling Leaderboard</h1>
 			<div className="leveling-container">
 				{stats.map((level) => (
-					<div key={level.i}>
-						<div className="leveling-item">
-							<h1
-								className="leveling__item-title"
-								style={{
-									background: getColour(level.i),
-								}}>
-								{level.i + 1}
-							</h1>
-							<p className="leveling__item-username">{level.level.tag}</p>
-							<CircularProgressbar
-								className="progress-ring"
-								value={getProgress(level)}
-								text={`Level ${level.level.level}`}
-								styles={buildStyles({
-									trailColor: "#50555A",
-									pathColor: "#46CBF1",
-									textColor: "#46CBF1",
-								})}
-							/>
-						</div>
-						{level.i + 1 < stats.length && <div className="break-line" />}
-					</div>
+					<Level key={level.i} {...level} breakline={level.i + 1 < stats.length} />
 				))}
 			</div>
 		</div>
