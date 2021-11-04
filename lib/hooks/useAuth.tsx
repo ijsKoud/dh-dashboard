cfeae436bcb9e8a5631ce8be39edfadeb300e10b
DaanGamesDG <daan@daangamesdg.wtf>
2021-11-04 16:32:26 +0100

    fix(UseAuth): /user/ not found

--- lib/hooks/useAuth.tsx
+++ lib/hooks/useAuth.tsx
@@ -24,7 +24,7 @@ const useProvideAuth = (): UseAuth => {
 	const [loading, setLoading] = useState<true | false>(true);
 
 	useEffect(() => {
-		fetch<User>("/user/", { method: "GET", withCredentials: true })
+		fetch<User>("/api/user/", { method: "GET", withCredentials: true })
 			.then((res) => {
 				setUser(res.data);
 				setLoading(false);
